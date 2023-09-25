3be7f8f442f1a8155e2a72d09a2344c1cb299396
EslamElsebaey <eslamelsebaey.11@gmail.com>
2023-09-25 12:21:56 +0300

    edit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,11 +7,11 @@ function Todo({todo , toggleDone , deleteTodo , editTodo , mode , bookmarkTodo})
       <div className="todos-todo_icon" onClick={()=>toggleDone(todo.id)}>  <FeatherIcon icon={todo.done ? "check-circle" : "circle"} size="22" />   </div>
       <div className="todos-todo_text"> {todo.title}</div>
       {mode !== "edit" ? (  <div className="todos-todo_cta">  
-      <div className="todos-todo_cta-edit" onClick={ ()=> editTodo(todo)}>
-      <FeatherIcon icon="edit" size="23" />
-      </div>
       <div onClick={()=> bookmarkTodo(todo.id)}  className="todos-todo_cta-bookmark">
       <FeatherIcon size="23" className={todo.pinned ? "isPinned" : ""}  icon="bookmark" />
+      </div>
+      <div className="todos-todo_cta-edit" onClick={ ()=> editTodo(todo)}>
+      <FeatherIcon icon="edit" size="23" />
       </div>
       <div onClick={()=> deleteTodo(todo.id)} className="todos-todo_cta-delete">
       <FeatherIcon icon="trash-2" size="23" />
